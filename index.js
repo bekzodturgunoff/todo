7d0281d081b03441d07fae42ae8838aec09810a5
Bekzod <60348530+bekzodturgunoff@users.noreply.github.com>
2022-01-05 17:45:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+//1 when I click add button
+// it shoud show another window 
+// to add my todo.
+// 2 when I click it it should underline
+// itself
+
+const buttonOne = document.querySelector("#btn1")
+const overlay = document.querySelector("#overlay")
+const closeOverlay = document.querySelector("#close-overlay")
+const model = document.querySelector("#model")
+
+buttonOne.addEventListener('click', () => {
+  overlay.classList.add("open")
+  model.classList.add("open")
+})
+
+closeOverlay.addEventListener("click", () => {
+  overlay.classList.remove("open")
+  model.classList.remove("open")
+})
+
