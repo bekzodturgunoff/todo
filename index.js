7a8c359e252d27adc8d19ed83d5a42abb2010090
Bekzod <60348530+bekzodturgunoff@users.noreply.github.com>
2022-01-08 17:56:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ const buttonOne = document.querySelector("#btn1")
 const buttonTwo = document.querySelector("#btn2")
 const overlay = document.querySelector("#overlay")
 const addTodo = document.querySelector(".add-todo")
+const makeBlur = document.querySelector("#make-blur")
+console.log(overlay)
 
 const form = document.querySelector("#form")
 const list = document.querySelector("#list")
 const input = document.querySelector("#input-t")
 
+const highP = document.querySelector(".high-b").checked = "false"
+const mediumP = document.querySelector(".medium-b").checked = "false"
+const lowP = document.querySelector(".low-b").checked = "false"
+
+
+
 buttonOne.addEventListener('click', () => {
   overlay.classList.add("open")
 
 })
+
+
 buttonTwo.addEventListener('click', () => {
   overlay.classList.add("open")
 
@@ -26,32 +36,30 @@ addTodo.addEventListener("click", () => {
 
 })
 
-const container = document.querySelector(".cont")
-
-const rightSide = container.children[1]
-
-const listItem = rightSide.children[2]
-
 form.addEventListener("submit", e => {
   e.preventDefault()
-
   const item = document.createElement("div")
+  // const date = document.querySelector(".overlay-date")
   item.innerText = input.value
+  // date.innerText = input.value
   item.classList.add("list-item")
+  // date.classList.add("list-item")
   list.appendChild(item)
-
+  // list.appendChild(date)
   input.value = ""
-  list.addEventListener("click", () => {
-    listItem.removeChild(item)
+
+
+  item.addEventListener("click", () => {
+    item.remove()
   })
 })
 
-
-
-
-
-
-
-
-
-
+  // (function () {
+  //   $('#submit').on('click', function () {
+  //     var date = new Date($('.overlay-date').val());
+  //     day = date.getDate();
+  //     month = date.getMonth() + 1;
+  //     year = date.getFullYear();
+  //     alert([day, month, year].join('/'));
+  //   });
+  // })();
