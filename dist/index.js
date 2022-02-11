79182e07faa092518a7953ac52deeff4cd4ff6ac
bekzodturgunoff <bekzodturgunoff@gmail.com>
2022-02-11 21:23:57 +0500

    some bugs fixed

--- dist/index.js
+++ dist/index.js
@@ -6,7 +6,7 @@ const root = document.getElementById("root");
 const links = document.querySelectorAll(".links");
 
 let routes = {
-  "/":homePage,
+  "/": homePage,
   "/overview": homePage,
   "/today": todayPage,
   "/important": importantPage,
